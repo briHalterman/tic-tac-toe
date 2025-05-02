701510281d6d7d2e66461a8ec23100bd17b3df5e
Brittany Halterman <halterma@gmail.com>
2025-05-02 09:38:26 -0400

    Initialize 3x3 board grid

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,10 @@
 class Board
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(3) { Array.new(3, " ") }
+  end
+
   def self.convert_coordinates_to_indices(letter, number)
     row = letter.upcase.ord - 65
     column = number - 1
--- main.rb
+++ main.rb
@@ -2,5 +2,8 @@ require_relative './lib/player'
 require_relative './lib/board'
 require_relative './lib/game'
 
+board = Board.new
+
 # Test
 puts Board.convert_coordinates_to_indices("B", 2) == [1, 1]
+puts board.grid.inspect
