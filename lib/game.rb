31ed5c4edd371a5c4c8d4f0f8c5d26e789a26a78
Brittany Halterman <halterma@gmail.com>
2025-05-02 20:41:35 -0400

    Initialize Game class with board, players, and starting player

---
+++ lib/game.rb
@@ -0,0 +1,8 @@
+class Game
+  def initialize
+    @board = Board.new
+    @player1 = Player.new("Player 1", "X")
+    @player2 = Player.new("Player 2", "O")
+    @current_player = @player1
+  end
+end
--- main.rb
+++ main.rb
@@ -4,21 +4,31 @@ require_relative './lib/player'
 require_relative './lib/board'
 require_relative './lib/game'
 
-board = Board.new
+game = Game.new
 
-player1 = Player.new("Player 1", "X")
-player2 = Player.new("Player 2", "O")
+# puts board.grid.inspect
 
-puts "#{player1.name}, choose a row:"
-row = gets.chomp.upcase
+# puts "#{player1.name}, choose a row:"
+# row = gets.chomp.upcase
 
-puts "#{player1.name}, choose a column:"
-column = gets.chomp.to_i
+# puts "#{player1.name}, choose a column:"
+# column = gets.chomp.to_i
 
-row, column = Board.convert_coordinates_to_indices(row, column)
-board.place_game_piece(player1.game_piece, row, column)
+# row, column = Board.convert_coordinates_to_indices(row, column)
+# board.place_game_piece(player1.game_piece, row, column)
+
+# puts board.grid.inspect
+
+# puts "#{player2.name}, choose a row:"
+# row = gets.chomp.upcase
+
+# puts "#{player2.name}, choose a column:"
+# column = gets.chomp.to_i
+
+# row, column = Board.convert_coordinates_to_indices(row, column)
+# board.place_game_piece(player2.game_piece, row, column)
+
+# puts board.grid.inspect
 
 # Test
-puts board.grid.inspect
-puts player1.inspect
-puts player2.inspect
+puts game.inspect
