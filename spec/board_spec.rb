0698752fb7334ab3e7e114cd5c5ebf531c950896
Brittany Halterman <halterma@gmail.com>
2025-05-09 09:33:49 -0400

    Test conversion of coordinates to indices

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,13 +5,20 @@ RSpec.describe Board do
   describe '#initialize' do
     it 'sets up a 3x3 grid' do
       board = Board.new
-      
+
       result = board.grid
       expect(result).to eq([
         [" ", " ", " "],
         [" ", " ", " "],
         [" ", " ", " "]
       ])
+    end
+  end
+
+  describe '.convert_coordinates_to_indices' do
+    it 'converts B, 2 to [1, 1]' do
+      result = Board.convert_coordinates_to_indices("B", 2)
+      expect(result).to eq([1, 1])
     end
   end
 end
